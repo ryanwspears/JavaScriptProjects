cd96911077bc9477f5d023e73754b4c5e01073b3
ryanwspears <spearswesley5@gmail.com>
2021-08-17 16:06:48 -0500

    Added code to main.js

--- Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
@@ -1,3 +1,36 @@
+// All of this code is just practicing vith variables.
+
+// Utilizing the window.alert() method
 window.alert("This is an alert.");
 
+// Utilizing the document.write() method
 document.write("Here is a line of text.");
+
+var a = "This is a string.";
+
+document.write(a);
+
+var stringText = "This is a line of text.";
+window.alert(stringText);
+
+var b = "\"Don\'t go into the woods.\", said Luke.";
+
+document.write("This is the first line of text." + "This is the second line of text.");
+
+var c = "Text one." + "Text two.";
+
+document.write(c);
+
+// Assigning values to variables
+var one = "One", two = "Two", three = "Three";
+
+// Assigning colors to variables
+var one = one.fontcolor("red");
+var two = two.fontcolor("orange");
+var three = three.fontcolor("blue");
+
+// Concatenating two variables
+document.write(two + one);
+
+// This is an expression
+document.write(5 + 5);
